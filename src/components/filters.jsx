6df6b583efcd64913356bd4f88cd7c17000774b4
Logan <loganmsorensen@gmail.com>
2021-02-03 06:25:41 -0800

    location field now updates based on user input

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import stays from "../data/stays.json";
 
 const Filters = () => {
+  const [rentalLocations, setRentalLocations] = useState([]);
+  const [location, setLocation] = useState(null);
   const [adultGuests, setAdultGuests] = useState(0);
   const [childGuests, setChildGuests] = useState(0);
   // const testFocus = (e) => {
   //     console.log('testing', e.target)
   // }
+
+  useEffect(() => {
+    const uniqueLocations = [];
+    stays.map((rental) => {
+      if (!uniqueLocations.includes(`${rental.city}, ${rental.country}`)) {
+        uniqueLocations.push(`${rental.city}, ${rental.country}`);
+        setRentalLocations(uniqueLocations);
+      }
+    });
+  }, []);
+
+  const selectLocation = (location) => {
+    //   console.log('hitting')
+    //   console.log(location)
+    setLocation(location);
+  };
 
   const addGuest = (e) => {
     if (e.target.name === "child") {
@@ -17,7 +37,6 @@ const Filters = () => {
 
   const removeGuest = (e) => {
     if (e.target.name === "child") {
-
       if (childGuests > 0) {
         setChildGuests(childGuests - 1);
       }
@@ -28,6 +47,8 @@ const Filters = () => {
     }
   };
 
+  console.log(rentalLocations);
+
   return (
     <div className="filters-active">
       <form>
@@ -37,6 +58,7 @@ const Filters = () => {
             type="text"
             id="location-input"
             placeholder="Select a location"
+            value={location !== null ? location : null}
           />
         </div>
         <div className="input-container">
@@ -63,18 +85,16 @@ const Filters = () => {
       </form>
       <div className="filter-options">
         <div className="location-options">
-          <div className="location-option">
-            <i className="fas fa-map-marker-alt"></i>Helsinki, Finland
-          </div>
-          <div className="location-option">
-            <i className="fas fa-map-marker-alt"></i>Turku, Finland
-          </div>
-          <div className="location-option">
-            <i className="fas fa-map-marker-alt"></i>Oulu, Finland
-          </div>
-          <div className="location-option">
-            <i className="fas fa-map-marker-alt"></i>Vaasa, Finland
-          </div>
+          {rentalLocations.map((location, index) => (
+            <div
+              key={index}
+              className="location-option"
+              onClick={() => selectLocation(location)}
+            >
+              <i className="fas fa-map-marker-alt"></i>
+              {location}
+            </div>
+          ))}
         </div>
         <div className="guest-options">
           <div className="guest-option">
